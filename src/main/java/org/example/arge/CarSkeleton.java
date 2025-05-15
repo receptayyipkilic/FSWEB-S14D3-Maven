ca969e3e4319d46f41a910138d1785c98047d453
receptayyipkilic <kilicrtayyip@gmail.com>
2025-05-15 20:49:45 +0300

    project completed

---
+++ src/main/java/org/example/arge/CarSkeleton.java
@@ -0,0 +1,34 @@
+package org.example.arge;
+
+public class CarSkeleton {
+    private String name;
+    private String description;
+
+    public CarSkeleton(){
+
+    }
+
+    public CarSkeleton(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void startEngine(){
+        System.out.println("Motor Çalıştır");
+    }
+    public void drive(){
+        System.out.println("Sürüş Başladı");
+        runEngine();
+    }
+    protected void runEngine(){
+        System.out.println("Motor çalışıyor");
+    }
+}
